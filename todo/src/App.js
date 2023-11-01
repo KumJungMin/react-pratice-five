3ab7df36868b478596fb7db7ead63b74f77b2744
KumJungMin <rmawjdals@naver.com>
2023-11-01 12:38:42 +0900

    chore: 초기 설정

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,5 @@
-import Router from "./Router";
-import CreateTodo from "./components/CreateTodo";
 function App() {
-  return (
-    <div className="App">
-      <CreateTodo />
-      <Router />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
