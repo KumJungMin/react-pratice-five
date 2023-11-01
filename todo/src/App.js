d907d566195e9237a0be26f8249bb635db0f3290
KumJungMin <rmawjdals@naver.com>
2023-11-02 08:58:34 +0900

    1. styledComponents로 전역 스타일 적용하기

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,5 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+
 function App() {
-  return <div className="App">1</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div className="App">1</div>
+    </>
+  );
 }
 
 export default App;
