9229cfc5ea632efe6108fc7ac37e642f349f68ca
KumJungMin <rmawjdals@naver.com>
2023-11-02 09:25:17 +0900

    chore: 할일 목록 컴포넌트 추가하기

---
+++ todo/src/components/TodoItem.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import { MdDone, MdDelete } from "react-icons/md";
+
+const Remove = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #dee2e6;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+  display: none;
+`;
+
+const ItemBlock = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  }
+`;
+
+const CheckCircle = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+  border: 1px solid #ced4da;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  cursor: pointer;
+`;
+
+const Text = styled.div`
+  flex: 1;
+  font-size: 21px;
+  color: #495057;
+`;
+
+interface TodoItemProps {
+  id: number;
+  done: boolean;
+  text: string;
+}
