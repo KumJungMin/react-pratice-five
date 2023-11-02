53338bc3f5c20226e00b04cefd75d57731f6dec8
KumJungMin <rmawjdals@naver.com>
2023-11-02 09:04:44 +0900

    2. header 추가(children 사용 방식)

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 import TodoTemplateBlock from "./components/TodoTemplate";
+import TodoHeader from "./components/TodoHeader";
+
 const GlobalStyle = createGlobalStyle`
   body {
     background: #e9ecef;
@@ -11,7 +13,10 @@ function App() {
     <>
       <GlobalStyle />
       <div className="App">
-        <TodoTemplateBlock />
+        {/* TodoTemplateBlock은 children을 받기에, 내부에 컴포넌트 지정 가능 */}
+        <TodoTemplateBlock>
+          <TodoHeader />
+        </TodoTemplateBlock>
       </div>
     </>
   );
