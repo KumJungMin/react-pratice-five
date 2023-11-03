2327c7ca74f56f0858e43ed3e283b3fab0b31588
KumJungMin <rmawjdals@naver.com>
2023-11-03 14:48:38 +0900

    8. useRef로 input 변경시 재런더링 방지하기(근데 제출 후 초기화안됨)

--- todo/src/components/CreateMemo.tsx
+++ todo/src/components/CreateMemo.tsx
@@ -1,25 +1,27 @@
 // 입력창(이름, 내용)을 사용해서 저장하면 -> MemoList에 추가되는 컴포넌트
-import { useState } from "react";
+import { useRef } from "react";
 
 function CreateMemo() {
-  const [name, setName] = useState("");
-  const [content, setContent] = useState("");
+  //  useState은 input값이 변경될 때마다, 컴포넌트가 리렌더링된다.
+  // 이를 방지하기위해, useRef를 사용한다.
+  // const [name, setName] = useState("");
+  // const [content, setContent] = useState("");
+  const title = useRef("");
+  const content = useRef("");
 
   function onchange(e: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = e.currentTarget;
-    if (name === "name") setName(value);
-    else setContent(value);
+    if (name === "title") title.current = value;
+    else if (name === "content") content.current = value;
   }
-
+  console.log(title.current, content.current);
   return (
     <form>
       <input
-        value={name}
-        name="name"
+        name="title"
         onChange={onchange}
       />
       <input
-        value={content}
         name="content"
         onChange={onchange}
       />
