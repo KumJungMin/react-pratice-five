973504ee81f68f79fe296864f30f4fec2823f11c
KumJungMin <rmawjdals@naver.com>
2023-11-03 13:24:51 +0900

    2. 코인별 메모 목록을 reducer로 관리하기

---
+++ todo/src/reducers/memo.ts
@@ -0,0 +1,48 @@
+import { useReducer } from "react";
+
+interface Memo {
+  id: string;
+  title: string;
+  content: string;
+}
+
+interface MemoList {
+  [key: string]: Memo[];
+}
+
+// 1. 초기값 지정하기
+export const initMemoList = {};
+
+interface IAction {
+  type: string;
+  payload: Memo;
+}
+
+// 2. 액션 만들기
+export function reducer(state: MemoList, action: IAction) {
+  switch (action.type) {
+    case "ADD_MEMO":
+      return addMemo(state, action.payload);
+    case "REMOVE_MEMO":
+      return removeMemo(state, action.payload.id);
+    default:
+      return state;
+  }
+}
+
+function addMemo(MemoList: MemoList, newMemo: Memo) {
+  const { id } = newMemo;
+  const currentValue = MemoList[id];
+  return {
+    ...MemoList,
+    [id]: currentValue ? [...currentValue, newMemo] : [newMemo],
+  };
+}
+
+function removeMemo(MemoList: MemoList, id: string) {
+  const currentValue = MemoList[id];
+  return {
+    ...MemoList,
+    [id]: currentValue.filter((memo) => memo.id !== id),
+  };
+}
