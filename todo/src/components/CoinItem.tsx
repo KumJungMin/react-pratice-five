f918851efcbcd411dd03f6c46e252e8016c5fdf4
KumJungMin <rmawjdals@naver.com>
2023-11-03 14:22:00 +0900

    5. Link로 라우터 이동시키기

--- todo/src/components/CoinItem.tsx
+++ todo/src/components/CoinItem.tsx
@@ -1,5 +1,6 @@
 import type { Coin } from "../hooks/useCoin";
 import React from "react";
+import { Link } from "react-router-dom";
 
 interface CoinItemProps {
   coin: Coin;
@@ -9,7 +10,15 @@ interface CoinItemProps {
 // !!리스트의 데이터의 적은 경우, React.memo를 사용하면 성능을 최적화할 수 있다.
 
 function CoinItem({ coin }: CoinItemProps) {
-  return <div>{coin.name}</div>;
+  return (
+    <div>
+      <p>{coin.name}</p>
+      {/* Link는 a태그와 비슷하다. */}
+      {/* a태그와 다른 점은 Link는 페이지를 새로고침하지 않고 페이지를 이동시켜준다. */}
+      {/* Link는 to라는 프로퍼티를 가진다. */}
+      <Link to={`/${coin.id}`}>상세보기</Link>
+    </div>
+  );
 }
 
 export default React.memo(CoinItem);
