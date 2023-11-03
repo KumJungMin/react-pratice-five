7f80f95f4488bdc5f43f1d05ede63a718e752c8d
KumJungMin <rmawjdals@naver.com>
2023-11-03 14:24:40 +0900

    7. useParams로 현재 URL의 파리미터값 사용 가능

--- todo/src/routes/CoinInfo.tsx
+++ todo/src/routes/CoinInfo.tsx
@@ -1,5 +1,14 @@
+// id를 받으면, 그 id값에 따라 vueQuery를 실행
+import { useParams } from "react-router-dom";
+interface RouteParams {
+  coinId: string;
+}
+
 function CoinInfo() {
-  return <>1</>;
+  // useParams는 url의 파라미터를 가져온다.
+  const { coinId } = useParams<RouteParams>();
+
+  return <>{coinId}</>;
 }
 
 export default CoinInfo;
