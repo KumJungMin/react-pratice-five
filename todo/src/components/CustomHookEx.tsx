e3252a3cbc8008b6fa596efe07edea236a0fb555
KumJungMin <rmawjdals@naver.com>
2023-11-06 14:30:01 +0900

    5-3. 커스텀 훅에 제출 후 동작 정의하기

--- todo/src/components/CustomHookEx.tsx
+++ todo/src/components/CustomHookEx.tsx
@@ -1,22 +1,17 @@
 import {useInput} from '../hooks/useInput';
 
+function displayMessage(message: string) {
+  alert(message);
+}
+
 
 function CustomHookEx() {
-  const {value, handleChange, reset} = useInput('안녕');
-  const {value: value2, handleChange: handleChange2, reset: reset2} = useInput('안녕2');
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    alert('submitted');
-    reset();
-    reset2();
-  }
-
+  const {value, handleChange, handleSubmit} = useInput({value: '안녕', submitAction: displayMessage});
+  
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input value={value} onChange={handleChange} />
-        <input value={value2} onChange={handleChange2} />
         
         <button type="submit">Submit</button>
       </form>
--- todo/src/hooks/useInput.ts
+++ todo/src/hooks/useInput.ts
@@ -1,20 +1,27 @@
 // value, onChange, reset
 import { useState } from 'react';
 
-export function useInput(value: string = '') {
+interface IUseInput {
+  value: string;
+  submitAction?: (value: string) => void;
+}
+
+export function useInput({value, submitAction}: IUseInput) {
   const [inputValue, setInputValue] = useState<string>(value);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   }
-  
-  const reset = () => {
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     setInputValue('');
+    submitAction && submitAction(inputValue);
   }
 
   return {
     value: inputValue,
     handleChange,
-    reset,
+    handleSubmit,
   }
 }
